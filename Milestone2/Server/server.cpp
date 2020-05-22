296d2d367c5995baf1ed070d3aab5016b1cffdbb
Hung Huynh <huynhh1@seattleu.edu>
2020-05-21 23:38:57 -0700

    finish merging

--- Milestone2/Server/server.cpp
+++ Milestone2/Server/server.cpp
@@ -10,9 +10,91 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-using namespace std;
-#define PORT 8675
+#include <iostream>
+#include <map>
+#include <string>
 
+#include "assert.h"
+using namespace std;
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////Mike code//////////////////////////////////////////////////////////////////////////
+class KeyValue {
+   private:
+    char m_szKey[128];
+    char m_szValue[2048];
+
+   public:
+    KeyValue(){};
+    void setKeyValue(char* pszBuff) {
+        char* pch1;
+
+        // find out where the "=" sign is, and take everything to the left of the equal for the key
+        // go one beyond the = sign, and take everything else
+        pch1 = strchr(pszBuff, '=');
+        assert(pch1);
+        int keyLen = (int)(pch1 - pszBuff);
+        strncpy(m_szKey, pszBuff, keyLen);
+        m_szKey[keyLen] = 0;
+        strcpy(m_szValue, pszBuff + keyLen + 1);
+    }
+
+    char* getKey() {
+        return m_szKey;
+    }
+
+    char* getValue() {
+        return m_szValue;
+    }
+};
+class RawKeyValueString {
+   private:
+    char m_szRawString[32768];
+    int m_currentPosition;
+    KeyValue* m_pKeyValue;
+    char* m_pch;
+
+   public:
+    RawKeyValueString(char* szUnformattedString) {
+        assert(strlen(szUnformattedString));
+
+        strcpy(m_szRawString, szUnformattedString);
+
+        m_pch = m_szRawString;
+    }
+    ~RawKeyValueString() {
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+    }
+
+    void getNextKeyValue(KeyValue& keyVal) {
+        // It will attempt to parse out part of the string all the way up to the ";", it will then create a new KeyValue object  with that partial string
+        // If it can;t it will return null;
+        char* pch1;
+        char szTemp[32768];
+
+        pch1 = strchr(m_pch, ';');
+        assert(pch1 != NULL);
+        int subStringSize = (int)(pch1 - m_pch);
+        strncpy(szTemp, m_pch, subStringSize);
+        szTemp[subStringSize] = 0;
+        m_pch = pch1 + 1;
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+        keyVal.setKeyValue(szTemp);
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Extract key and value as a pair<key,value>
+@param pRawKey raw input
+@param rpcKeyValue key value pair
+@return pair<key,value>
+*/
+pair<char*, char*> extractKeyValue(RawKeyValueString* pRawKey, KeyValue& rpcKeyValue) {
+    pRawKey->getNextKeyValue(rpcKeyValue);
+    return {rpcKeyValue.getKey(), rpcKeyValue.getValue()};
+}
 // This class will perform the various network Operations to set up the server
 
 class ServerContextData {
@@ -24,7 +106,7 @@ class ServerContextData {
     pthread_cond_t fill;
     int nMaxAmount;
 
-public:
+   public:
     ServerContextData() {
         connectionAmount = 0;
         rpcAmount = 0;
@@ -73,7 +155,7 @@ public:
 class ConnectionContextData {
     // You will put in your own "Global Data" that you will share among all the various client connections you have. Change your "getters" to reflect that
 
-public:
+   public:
     ConnectionContextData() {
         rpcAmount = 0;
     }
@@ -94,19 +176,19 @@ public:
         return sumAmount;
     }
 
-private:
+   private:
     int rpcAmount = 0;
     int sumAmount = 0;
 };
 
 class Server {
-private:
+   private:
     int m_server_fd;
     struct sockaddr_in m_address;
     int m_addrlen = sizeof(m_address);
     int m_port;
 
-public:
+   public:
     Server(int nPort) {
         m_port = nPort;
     }
@@ -125,7 +207,7 @@ public:
 
         // Forcefully attaching socket to the port 8080
         if (setsockopt(m_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-            &opt, sizeof(opt))) {
+                       &opt, sizeof(opt))) {
             perror("setsockopt");
             exit(EXIT_FAILURE);
         }
@@ -135,8 +217,8 @@ public:
         m_address.sin_port = (uint16_t)htons((uint16_t)m_port);
 
         // Forcefully attaching socket to the port 8080
-        if (bind(m_server_fd, (struct sockaddr*) & m_address,
-            sizeof(m_address)) < 0) {
+        if (bind(m_server_fd, (struct sockaddr*)&m_address,
+                 sizeof(m_address)) < 0) {
             perror("bind failed");
             exit(EXIT_FAILURE);
         }
@@ -153,8 +235,8 @@ public:
     int acceptNewConnection() {
         int new_socket;
 
-        if ((new_socket = accept(m_server_fd, (struct sockaddr*) & m_address,
-            (socklen_t*)&m_addrlen)) < 0) {
+        if ((new_socket = accept(m_server_fd, (struct sockaddr*)&m_address,
+                                 (socklen_t*)&m_addrlen)) < 0) {
             perror("accept");
             return (-1);
         }
@@ -163,7 +245,7 @@ public:
 
     int chatter(int new_socket) {
         int valread;
-        char buffer[1024] = { 0 };
+        char buffer[1024] = {0};
         const char* hello = "Hello from server";
         valread = (int)read(new_socket, (void*)buffer, (size_t)1024);
         printf("%s Bytes read = %d\n", buffer, valread);
@@ -181,35 +263,91 @@ public:
     }
 };
 
+int rpcConnect(char* pszUserName, char* pszPass) {
+    map<string, string> hashMap;
+    hashMap["sam"] = "123";
+    hashMap["corey"] = "123";
+    hashMap["hung"] = "123";
+    hashMap["mike"] = "123";
+    const char* pass = hashMap[pszUserName].c_str();
+    if (strcmp(pass, pszPass) == 0) {
+        return 1;
+    }
+
+    return 0;
+}
 void* rpcThread(void* arg) {
-    int nSocket = *(int*)socket;
+    int new_socket = *(int*)socket;
     int valread;
-    char buffer[1024] = { 0 };
+    char buffer[1024] = {0};
     void* status = NULL;
 
     // We will  block when there are too many connections. Lets say 100
     ServerContextData* pServerContextData = (ServerContextData*)arg;
-    nSocket = pServerContextData->getSocket();
+    new_socket = pServerContextData->getSocket();
     ConnectionContextData* connectionContextDataObj = new ConnectionContextData();
+
     for (;;) {
-        const char* statusOK = "STATUS=OK";
-        valread = (int)read(nSocket, (void*)buffer, (size_t)1024);
-        if (strcmp(buffer, "QUIT") == 0) {
-            connectionContextDataObj->addRpcAmount();
-            pServerContextData->incrementTotalRpc();
-            printf("Client with socket %d is leaving.\nTotal Server RPC Count: %d\n", nSocket, pServerContextData->getTotalRpc());
-            pthread_exit(status);
+        valread = (int)read(new_socket, (void*)buffer, (size_t)1024);
+        if (valread != 0) {
+            RawKeyValueString* pRawKey = new RawKeyValueString((char*)buffer);
+            KeyValue rpcKeyValue;
+            pair<char*, char*> rpc = extractKeyValue(pRawKey, rpcKeyValue);
+
+            if (strcmp(rpc.second, "connect") == 0) {
+                KeyValue user, pass;
+                pair<char*, char*> userKeyValue = extractKeyValue(pRawKey, user);
+                pair<char*, char*> passKeyValue = extractKeyValue(pRawKey, pass);
+
+                if (rpcConnect(userKeyValue.second, passKeyValue.second)) {
+                    cout << userKeyValue.second << " is connected now...\n";
+                    send(new_socket, "\nWelcome from server!", strlen("welcome from server!\n"), 0);
+                } else {
+                    cout << "client username or password incorrect" << endl;
+                    send(new_socket, "Not Authorized", strlen("Not Authorized"), 0);
+                }
+            }
+
+            else if (strcmp(rpc.second, "1") == 0) {
+                send(new_socket, "implement view List", strlen("implement view List"), 0);
+            }
+
+            else if (strcmp(rpc.second, "2") == 0) {
+                send(new_socket, "implement view cart", strlen("implement view cart"), 0);
+            }
+
+            else if (strcmp(rpc.second, "3") == 0) {
+                send(new_socket, "implement add cart", strlen("implement add cart"), 0);
+            }
+
+            else if (strcmp(rpc.second, "4") == 0) {
+                send(new_socket, "implement remove from cart", strlen("implement remove from cart"), 0);
+            }
+
+            else {
+                printf("Client with socket %d is leaving.\n", new_socket);
+                pthread_exit(status);
+            }
+
+            delete pRawKey;
         }
 
-        int incAmt = atoi((char const*)buffer);
-        connectionContextDataObj->addSumAmount(incAmt);
-        connectionContextDataObj->addRpcAmount();
-        pServerContextData->incrementTotalRpc();
-        printf("%s Bytes read = %d  from socket %d\n", buffer, valread, nSocket);
-        printf("sumAmt=%d  ThreadRPCAmt=%d\n", connectionContextDataObj->getSumAmount(), connectionContextDataObj->getRpcAmount());
-        send(nSocket, statusOK, strlen(statusOK), 0);
+        // if (strcmp(buffer, "QUIT") == 0) {
+        //     connectionContextDataObj->addRpcAmount();
+        //     pServerContextData->incrementTotalRpc();
+        //     printf("Client with socket %d is leaving.\nTotal Server RPC Count: %d\n", nSocket, pServerContextData->getTotalRpc());
+        //     pthread_exit(status);
+        // }
 
-        printf("Response message sent.\n");
+        // int incAmt = atoi((char const*)buffer);
+        // connectionContextDataObj->addSumAmount(incAmt);
+        // connectionContextDataObj->addRpcAmount();
+        // pServerContextData->incrementTotalRpc();
+        // printf("%s Bytes read = %d  from socket %d\n", buffer, valread, nSocket);
+        // printf("sumAmt=%d  ThreadRPCAmt=%d\n", connectionContextDataObj->getSumAmount(), connectionContextDataObj->getRpcAmount());
+        // send(nSocket, statusOK, strlen(statusOK), 0);
+
+        // printf("Response message sent.\n");
     }
     fflush(stdout);
 
@@ -223,10 +361,10 @@ int main(int argc, char const* argv[]) {
     Server* serverObj = new Server(nPort);
     ServerContextData* serverContextDataObj;
     serverContextDataObj = new ServerContextData();
-    int rpcAmt = serverContextDataObj->getTotalRpc();
-    printf("Total Server RPC Count: %d\n", rpcAmt);
 
+    //start the server
     serverObj->startServer();
+
     do {
         int newSocket = serverObj->acceptNewConnection();
         if (newSocket <= 0) {
@@ -239,161 +377,3 @@ int main(int argc, char const* argv[]) {
         printf("Server accepted one thread. On to another!\n");
     } while (status == 0);
 }
-
-#if 0
-/*
-    int status = 0;
-    do
-    {
-        pthread_t p1, p2;
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-            (socklen_t*)&addrlen)) < 0)
-        {
-            perror("accept error");
-            status = -1;
-        }
-        printf("Accepted Connected");
-        int newClientSocket = new_socket;
-        pthread_create(&p1, NULL, serverThread, &newClientSocket);
-    } while (status == 0);
-
-
-
-}
-
-
-
-void *serverThread(void *arg)
-{
-    int socket = *(int *) arg;
-    pthread_mutex_lock(&global_mutex);
-
-    pthread_mutex_unlock(&global_mutex);
-}
-
-
-
-
-void *serverThread(void *arg) {
-    int i;
-
-    for (i = 0; i < loops; i++) {
-        pthread_mutex_lock(&count_mutex);
-        counter++;
-        pthread_mutex_unlock(&count_mutex);
-    }
-
-    return NULL;
-}
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        fprintf(stderr, "usage threads <value > \n");
-        exit(-1);
-    }
-
-
-    loops = atoi(argv[1]);
-    pthread_t p1, p2;
-    pthread_create(&p1, NULL, worker, NULL);
-    pthread_create(&p2, NULL, worker, NULL);
-    pthread_join(p1, NULL);
-    pthread_join(p2, NULL);
-    printf("Final value : %d\n", counter);
-    return 0;
-*/
-
-
-
-// We need to guard the 
-
-
-
-//#define PORT 8080
-#define PORT 8082
-/*
-int main(int argc, char const *argv[])
-{
-
-
-    int server_fd, new_socket;
-    ssize_t valread;
-
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    const char *hello = "Hello from server";
-    printf("Hello world\n");
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Got Socket\n");
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-        &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Forcefully attaching socket to the port 8080
-    printf("About to bind\n");
-    if (bind(server_fd, (struct sockaddr *)&address,
-        sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    printf("Waiting");
-
-    int status = 0;
-    do
-    {
-        pthread_t p1, p2;
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-            (socklen_t*)&addrlen)) < 0)
-        {
-            perror("accept error");
-            status = -1;
-        }
-        printf("Accepted Connected");
-        int newClientSocket = new_socket;
-        pthread_create(&p1, NULL, serverThread, &newClientSocket);
-    } while (status == 0);
-
-
-    printf("Only reach this in an error condition");
-
-    // We will create a new thread and pass the new_socket to the thread
-    // Thread will receive some type of notification that client disconnected and will terminate
-    // we don't have to worry about our children (Threads). Yes we are a bad parent. We simply worry about always taking new connections
-
-
-    // We will read the very simple HELLO message and return a Hello message back
-
-    valread = read(new_socket, buffer, 1024);
-    printf("%s Bytes read = %d\n", buffer, valread);
-    // What RPC is it
-    // Parse out arguments
-    // Call the correct RPC Function
-
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    return 0;
-}
-
-*/
-#endif
--- Milestone2/client/client.cpp
+++ Milestone2/client/client.cpp
@@ -1,119 +1,250 @@
 
 
-// Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
+// Client side C/C++ program to demonstrate Socket programming
+#include <arpa/inet.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <string>
+#include <thread>
 
-int connectRPC()
-{
-	// Input Arguments are:
-	// username
-	// password
-	// input format="rpc=connect;username=<Your user>;password=<Your password>;"
-	// Output arguments are:
-	// status     (This will be set to 1 if success and -1 if error)
-	// error      ( This will be to blank or an error message)
-	// output format="status=<error status>;error=<error or blank>
-	return 0;
+#include "assert.h"
+using namespace std;
+int valread;
+char buffer[1024] = {0};
+//Mike's code
+class KeyValue {
+   private:
+    char m_szKey[128];
+    char m_szValue[2048];
+
+   public:
+    KeyValue(){};
+    void setKeyValue(char* pszBuff) {
+        char* pch1;
+
+        // find out where the "=" sign is, and take everything to the left of the equal for the key
+        // go one beyond the = sign, and take everything else
+        pch1 = strchr(pszBuff, '=');
+        assert(pch1);
+        int keyLen = (int)(pch1 - pszBuff);
+        strncpy(m_szKey, pszBuff, keyLen);
+        m_szKey[keyLen] = 0;
+        strcpy(m_szValue, pszBuff + keyLen + 1);
+    }
+
+    char* getKey() {
+        return m_szKey;
+    }
+
+    char* getValue() {
+        return m_szValue;
+    }
+};
+
+// This class will take a string that is passed to it in this format:
+
+// input to constructor:
+// <variable1>=<value1>;<variable2>=<value2>;
+//You will then call the method  getNextKeyValue until getNextKeyValue returns NULL.
+// getNextKeyValue will return a KeyValue object. Inside of that KeyValue object will contain the variable and the value
+// You will then call getKey or getValue to get the contents of those fields.
+// The example in main() will show how to call this function.
+// By extracting the contents you then can determine the rpc you need to switch to, along with variables you will need
+// You can also use this class in your client program, since you will need to determine the contents that you receive from server
+
+class RawKeyValueString {
+   private:
+    char m_szRawString[32768];
+    int m_currentPosition;
+    KeyValue* m_pKeyValue;
+    char* m_pch;
+
+   public:
+    RawKeyValueString(char* szUnformattedString) {
+        assert(strlen(szUnformattedString));
+        strcpy(m_szRawString, szUnformattedString);
+
+        m_pch = m_szRawString;
+    }
+    ~RawKeyValueString() {
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+    }
+
+    void getNextKeyValue(KeyValue& keyVal) {
+        // It will attempt to parse out part of the string all the way up to the ";", it will then create a new KeyValue object  with that partial string
+        // If it can;t it will return null;
+        char* pch1;
+        char szTemp[32768];
+
+        pch1 = strchr(m_pch, ';');
+        assert(pch1 != NULL);
+        int subStringSize = (int)(pch1 - m_pch);
+        strncpy(szTemp, m_pch, subStringSize);
+        szTemp[subStringSize] = 0;
+        m_pch = pch1 + 1;
+        if (m_pKeyValue)
+            delete (m_pKeyValue);
+        keyVal.setKeyValue(szTemp);
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Connnect Rpc
+int connectRPC(int& sock) {
+    string option;
+    cout << "Do You want to connect to the server?\n";
+
+    while (true) {
+        memset(buffer, 0, 1024);
+        cout << "\nMenu:\n1.Connect\n2.Disconnect" << endl;
+        cout << "Enter number for option: ";
+        getline(cin, option);
+        if (option.compare("1") == 0) {
+            string username;
+            string password;
+            cout << "\nEnter your username: ";
+            getline(cin, username);
+            cout << "Enter your password: ";
+            getline(cin, password);
+
+            string rpcMessage = "rpc=connect;user=" + username + ";password=" + password + ";";
+
+            const char* messageTOserver = rpcMessage.c_str();
+            send(sock, messageTOserver, strlen(messageTOserver), 0);
+            cout << "\nServer is busy. Please wait ..." << endl;
+            valread = read(sock, buffer, 1024);
+
+            if (strcmp(buffer, "Not Authorized") == 0) {
+                cout << buffer;
+                cout << "\n";
+            } else {
+                cout << buffer;
+                cout << "\n";
+                return 1;
+            }
+        } else if (option.compare("2") == 0) {
+            break;
+        } else {
+            cout << "Invalid option!\n";
+        }
+    }
+    return 0;
 }
 
-int disconnectRPC()
-{
-	// input format="rpc=disconnect"
-	// output format="status=<error status>; error=<error or blank>;
-	return 0;
+/*
+DisconnectRPC
+*/
+int disconnectRPC(int& sock) {
+    send(sock, "rpc=5;", strlen("rpc=5;"), 0);
+    valread = read(sock, buffer, 1024);
+    cout << buffer << endl;
+    return 0;
 }
 
-int foobarRPC()
-{
-	return 0;
+int incrementRPC(int& sock, char* buff) {
+    size_t valRead = 0;
+    char hello[24];
+    strcpy(hello, "Hello from client");
+
+    char buffer[1024] = {0};
+    send(sock, buff, strlen(buff), 0);
+    printf("Hello message sent\n");
+    if (strcmp(buff, "QUIT") == 0) {
+        return 0;
+    } else {
+        valRead = read(sock, buffer, 1024);
+        printf("ValRead=%lu buffer=%s\n", valRead, buffer);
+    }
+    return 0;
 }
 
-int incrementRPC(int& sock, char* buff)
-{
-	size_t valRead = 0;
-	char hello[24];
-	strcpy(hello, "Hello from client");
+int connectToServer(char* szHostName, char* szPort, int& sock) {
+    struct sockaddr_in serv_addr;
 
-	char buffer[1024] = { 0 };
-	send(sock, buff, strlen(buff), 0);
-	printf("Hello message sent\n");
-	if (strcmp(buff, "QUIT") == 0) {
-		return 0;
-	}
-	else {
-		valRead = read(sock, buffer, 1024);
-		printf("ValRead=%lu buffer=%s\n", valRead, buffer);
-	}
-	return 0;
+    serv_addr.sin_family = AF_INET;
+    uint16_t port = (uint16_t)atoi(szPort);
+
+    serv_addr.sin_port = htons(port);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return 0;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, szHostName, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return 0;
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return 0;
+    }
+
+    return 1;
 }
 
-int connectToServer(char* szHostName, char* szPort, int& sock)
-{
-
-	struct sockaddr_in serv_addr;
-
-	serv_addr.sin_family = AF_INET;
-	uint16_t port = (uint16_t)atoi(szPort);
-
-	serv_addr.sin_port = htons(port);
-
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\n Socket creation error \n");
-		return -1;
-	}
-
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port);
-
-
-	if (inet_pton(AF_INET, szHostName, &serv_addr.sin_addr) <= 0)
-	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
-	}
-
-	if (connect(sock, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) < 0)
-	{
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-
-	return 0;
+int disconnectServer(int& sock) {
+    send(sock, "rpc=5;", strlen("rpc=5;"), 0);
+    close(sock);
+    return 0;
 }
 
+//Menu item for user
+int menu(int& sock) {
+    string option;
 
-int disconnectServer(int sock)
-{
-	int status;
-	status = close(sock);
-	return status;
+    cout << "\nMenu:\n1.View item List\n 2.View your cart\n 3.Add item to cart\n 4.remove from list\n 5.discconect " << endl;
+    cout << "Enter your option: ";
+    getline(cin, option);
+    if (option == "1") {
+        send(sock, "rpc=1;", strlen("rpc=1;"), 0);
+        valread = read(sock, buffer, 1024);
+        cout << buffer << endl;
+
+    } else if (option == "2") {
+        send(sock, "rpc=2;", strlen("rpc=2;"), 0);
+        valread = read(sock, buffer, 1024);
+        cout << buffer << endl;
+    } else if (option == "3") {
+        send(sock, "rpc=3;", strlen("rpc=3;"), 0);
+        valread = read(sock, buffer, 1024);
+        cout << buffer << endl;
+    } else if (option == "4") {
+        send(sock, "rpc=4;", strlen("rpc=4;"), 0);
+        valread = read(sock, buffer, 1024);
+        cout << buffer << endl;
+
+    } else if (option == "5") {
+        return disconnectServer(sock);
+
+    } else
+        cout << "Invalid option!\n";
+
+    return 1;
 }
+int main(int argc, char const* argv[]) {
+    int sock = 0;
+    int status = 0;
+    char buff[128];
 
-
-int main(int argc, char const* argv[])
-{
-	int sock = 0;
-	int status;
-	char buff[128];
-	// We will find out how many times to send our hello
-
-	status = connectToServer((char*)argv[1], (char*)argv[2], sock);
-
-	strcpy(buff, "1");
-	incrementRPC(sock, buff);
-	strcpy(buff, "15");
-	incrementRPC(sock, buff);
-	strcpy(buff, "QUIT");
-	incrementRPC(sock, buff);
-
-	status = disconnectServer(sock);
-
-	return status;
+    status = connectToServer((char*)argv[1], (char*)argv[2], sock);
+    int temp = connectRPC(sock);
+    while (status && temp) {
+        status = menu(sock);
+    }
+    return 0;
 }
